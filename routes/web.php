7c456dde36b82dd0cb134d0dbacea3194a3e4fbb
dariocasx <58869926+dariocasx@users.noreply.github.com>
2021-04-26 04:44:13 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,25 @@
 |
 */
 
-Route::resource('clientes', 'ClienteController', ['except' => ['create']]);
+Route::resource('clientes', 'ClienteController', ['except' => ['create']]); //vista del crud de cliente
+Route::resource('grupos','GrupoController');//vista del crud de grupos
+Route::resource('layout','LayoutController');//vista del crud de grupos
 
-Route::get('table', 'ClienteController@getTable');
-Route::get('table2', 'GrupoController@getTable');
 
-Route::resource('grupos','GrupoController');
+Route::get('table', 'ClienteController@getTable');//tabla de cliente
+Route::get('table2', 'GrupoController@getTable');//tabla de grupo
+
+
 
  Route::get('/', function () {
-     return redirect()->route('clientes.index');
- 	//return view('clientes.index');
+     //return redirect()->route('layout.index');
+ 	return view('layout.index');
  });
 
- Route::resource('customsearch', 'CustomSearchController');
- //Route::get('/clientes', 'ClienteController@index');
- Route::get('/clientes/action', 'ClienteController@action')->name('clientes.action');
+ Route::resource('customsearch', 'CustomSearchController');//controlador de las busquedas de cliente y grupo
+
+ 
+ //Route::get('action', 'ClienteController@action')->name('clientes.action');
 
 //Route::redirect('/', '/clientes', 301);
 
