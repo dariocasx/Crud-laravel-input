108c3bcc594e2f5cc004185273661ffb237fea1e
dariocasx <58869926+dariocasx@users.noreply.github.com>
2021-04-27 03:00:57 -0300

    Add files via upload

--- resources/views/grupos/index.blade.php
+++ resources/views/grupos/index.blade.php
@@ -3,7 +3,7 @@
 <!-- Todos los js para los request de los controladores-->
 <script src="{{ asset('js/refresh.js') }}"></script>
 <script src="{{ asset('js/store.js') }}"></script>
-<!--<script src="{{ asset('js/edit.js') }}"></script>-->
+<script src="{{ asset('js/edit.js') }}"></script>
 <script src="{{ asset('js/update.js') }}"></script>
 <script src="{{ asset('js/show.js') }}"></script>
 <script src="{{ asset('js/destroy.js') }}"></script>
@@ -47,61 +47,4 @@
 			});
 		 });
 
-
-jQuery(document).ready(function($)  {
-	
-	$(".table-container2").on("click touchstart", ".edit-btn", function () {
-		$.ajax({
-		    type: "GET",
-		    url: "grupos/" + $(this).attr("value") + "/edit",
-		    dataType: 'json',
-			headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-			beforeSend: function() {
-			$('#item-not-found').remove();
-			},
-		    success: function (data) {
-				$("#update-id2").val(data['id']);
-				$("#update-nombre_grupo").val(data['nombre']);
-				$('#update-form2').show();
-			},
-		    error: function(data) {
-				$.notify({
-					// opciones
-					icon: 'fa fa-exclamation-triangle',
-					title: '<strong>Error</strong>: <br>',
-					message: 'Ocurrio un error obteniendo los datos.'
-					},{
-					// parametros
-					type: "danger",
-					allow_dismiss: true,
-					newest_on_top: true,
-					showProgressbar: false,
-					placement: {
-					from: "top",
-					align: "right"
-					},
-					offset: 20,
-					spacing: 10,
-					z_index: 9999,
-					delay: 5000,
-					timer: 1000,
-					mouse_over: "pause",
-					animate: {
-					enter: 'animated fadeInDown',
-					exit: 'animated fadeOutUp'
-					}
-				});
-				$('#update-form2').hide();
-				(function(){
-				var notFound = $('<div class="modal-body fade-text" id="item-not-found"><h1 class="text-center danger">☠</h1><h2 class="text-center">Item not found</h2></div>');
-				notFound.insertAfter('#update-form');
-				})();
-	    	}, complete() {
-	    		$('#edit-loading-bar2').hide();
-	    	}
-		});
-	});
-});
-
 </script>
-
