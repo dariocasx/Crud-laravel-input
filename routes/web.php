bf2637ac92ed2e4954c7801e2a8f834ea92ad521
dariocasx <58869926+dariocasx@users.noreply.github.com>
2021-04-28 02:23:03 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -13,24 +13,15 @@
 
 Route::resource('clientes', 'ClienteController', ['except' => ['create']]); //vista del crud de cliente
 Route::resource('grupos','GrupoController');//vista del crud de grupos
-Route::resource('layout','LayoutController');//vista del crud de grupos
-
 
 Route::get('table', 'ClienteController@getTable');//tabla de cliente
 Route::get('table2', 'GrupoController@getTable');//tabla de grupo
 
+Route::view('home', 'layout/home');//layout del home
+Route::get('/', function () {
+ 	return view('layout.index');//vista del home 
+});
+
+Route::resource('customsearch', 'CustomSearchController');//controlador de las busquedas de cliente y grupo
 
 
- Route::get('/', function () {
-     //return redirect()->route('layout.index');
- 	return view('layout.index');
- });
-
- Route::resource('customsearch', 'CustomSearchController');//controlador de las busquedas de cliente y grupo
-
- 
- //Route::get('action', 'ClienteController@action')->name('clientes.action');
-
-//Route::redirect('/', '/clientes', 301);
-
-//Auth::routes();
